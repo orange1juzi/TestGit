934353f75d574e2a3fcd0476783d52b7bd77814a
as <654321>
2019-10-12 19:03:20 +0800

    br

--- src/main/java/com/atguigu/hello/Hello.java
+++ src/main/java/com/atguigu/hello/Hello.java
@@ -3,10 +3,6 @@ package com.atguigu.hello;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("test1");
-		System.out.println("test2");
-		System.out.println("test3");
-		
 		System.out.println("br");
 	}
 }
