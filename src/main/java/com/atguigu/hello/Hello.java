ad711907beb2a55ff4450df0fd4c084a3dfb503b
as <654321>
2019-10-12 18:34:56 +0800

    "test1"

--- src/main/java/com/atguigu/hello/Hello.java
+++ src/main/java/com/atguigu/hello/Hello.java
@@ -4,9 +4,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("test1");
-		System.out.println("test2");
-		System.out.println("test3");
-		
+
 		System.out.println("br");
 	}
 }
